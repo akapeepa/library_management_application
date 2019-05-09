50f0c549183b3205dfd7cdd2bc0034cd3903fbef
Gurdeep Singh <gurdeep.eec70@gmail.com>
2019-05-09 00:55:37 -0400

    initail commit
    this is the initial commit to this repo

---
+++ library_management_application/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Book
+{
+    int code;    char title[15];    char author[15] ; char isbn[15]; float price;
+};
+//---------------- START OF MAIN-----------------------------------
